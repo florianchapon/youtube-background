3eaef6ff1adf63241ff60e18b4f7a623d57e5e59
Florian CHAPON <chapon.florian@gmail.com>
2016-02-20 15:56:17 +0100

    Add youtube-background rep
    Add youtube-background rep

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<?
+// jQuery youtube-background plugin
+// by Florian CHAPON
+// http://www.florian-chapon.fr/
+// version: 1.0
+// updated: Febrary 19, 2016
+// since 20166
+// Enjoy.
+//
+// Florian 
+?>
+<html>
+<title>Youtube-Background</title>
+<head>
+<!-- Jquery Lib -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+<!-- Our script -->
+<script src="ytbg.js"></script>
+</head>
+
+<body>
+<!-- Personal parameters -->
+<script>
+$(document).ready(function() {	
+	ytbg("vQWlNALvbhE", 0, 17, 1);
+});
+</script>
+<!-- No margin, no padding -->
+<style>body {margin:0; padding:0; }</style>
+</body>
+
+</html>
---
+++ ytbg.js
@@ -0,0 +1,106 @@
+// jQuery youtube-background plugin
+// by Florian CHAPON
+// http://www.florian-chapon.fr/
+// version: 1.0
+// updated: Febrary 19, 2016
+// since 20166
+// Enjoy.
+//
+// Florian 
+
+
+$(document).ready(function () {
+	var div = "<div id='front-background' style='position:fixed;width:100%;height:100%;z-index:2;'></div><div id='yt-background' style='position:fixed; width:100%; height:100%; z-index:1;'><div id='ytplayer' style='position: absolute;'</div>";
+	$("body").prepend(div);
+});
+
+
+ var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var player;
+	
+	// Default value 
+	var embedId = "Kdna9gskdG4";
+	var starttime = 6; 
+	var endtime = 17;
+	var setvolume = "0";
+	
+	
+	function ytbg(url, start, end, vol){
+		embedId = url;
+		starttime = start;
+		endtime = end;
+		setvolume = vol;
+	}
+	
+
+	
+	
+	function onYouTubeIframeAPIReady() {
+        player = new YT.Player('ytplayer', {
+			videoId: embedId,
+            playerVars:{
+				controls:0,
+				showinfo:0,
+				modestbranding:1,
+				autoplay: 1,
+				start:starttime,
+				end:endtime,	
+				rel: 0,
+				wmode:"transparent"
+			},
+			
+			events: {
+                onReady:onPlayerReady,
+				onStateChange:onPlayerStateChange
+            }
+        });
+    }
+	
+	
+	
+    function onPlayerReady() {
+        player.playVideo();
+		player.setVolume(setvolume);
+        // Mute!
+        // player.mute();	
+    }
+	function onPlayerStateChange(e) {
+		 if (e.data === YT.PlayerState.ENDED) {
+            player.seekTo(starttime);
+		}
+	}	
+	
+	
+setInterval(function(){
+		
+	// Get viewport informations 
+	var height = $( window ).height();
+	var width = $( window ).width();
+	
+	// Calculate height and width considering 16/9 ratio 
+	var calcH = height * (16 / 9);
+	var calcW = width * (9 / 16);
+	
+	// Calculate left and top position to center player 
+	var left = (width - calcH) / 2;
+	var top = (height - calcW) / 2;
+	
+	// Responsive player, size and position 	
+	if ( width < calcH) {
+		$("#ytplayer").css("width", calcH);
+		$("#ytplayer").css("height", height);
+		$("#ytplayer").css("left", left);		
+	}
+	
+	else { 
+		$("#ytplayer").css("width", width);
+		$("#ytplayer").css("height", calcW);
+		$("#ytplayer").css("top",top);
+	}
+	
+},100);		
+		
+	
